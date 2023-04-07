c25c9cdde108cd88b32386d236644230e035f844
gweskaine <dianacris.tobon.m@gmail.com>
2023-04-07 12:33:44 -0500

    build e-store using json server

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {  
+  const[results, setResults] = React.useState([]);
+  React.useEffect(()=>{
+    fetch("http://localhost:3001/categories")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setResults(data);
+    })
+
+  }, [])
+
+    return (
+      <div className="App">        
+        {
+          results.map(d => (
+            <div key={d.id}>{d.title}</div>
+          ))
+        }
+      </div>
+    );  
+}
+
+export default App;
